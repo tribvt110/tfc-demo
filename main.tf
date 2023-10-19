c31f71e709981aff42fc6afbdcbedd2a252669fa
tribvt110 <148415336+tribvt110@users.noreply.github.com>
2023-10-19 12:46:45 +0700

    update

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-resource "null_resource" {}
+# main.tf
+
+# Provider configuration (e.g., AWS, Azure, etc.)
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Null resource definition
+resource "null_resource" "example" {
+  # Triggers the resource to be recreated if the value of `message` changes
+  triggers = {
+    message = "${var.message}"
+  }
+
+  # Provisioner block to execute commands
+  provisioner "local-exec" {
+    command = "echo ${var.message}"
+  }
+}
+
+# Variable definition
+variable "message" {
+  type    = string
+  default = "Hello, Terraform!"
+}
